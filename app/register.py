1e1908608ed6916933a4d7b6c2293ceb23556eed
YueYingLee <yueyinglee@gmail.com>
2023-04-25 13:47:19 -0700

    Fixed syntax error

--- app/register.py
+++ app/register.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
-class registerForm(FlaskForm):
+class registerUser(FlaskForm):
  fullname = StringField('Full Name', validators = [DataRequired(), Length (min = 4, max = 10)])
  username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)])
  password = PasswordField('New password', validators = [DataRequired(), Length (min=4, max =10),EqualTo('confirm', message='Passwords must match')])
