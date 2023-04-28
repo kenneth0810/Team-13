56a18fa90ba98d87bd44ca5f020a8b24280def00
YueYingLee <yueyinglee@gmail.com>
2023-04-28 14:34:28 -0700

    modified the option to add error messages

--- app/register.py
+++ app/register.py
@@ -6,6 +6,6 @@ class registerUser(FlaskForm):
  fullname = StringField('Full Name', validators = [DataRequired(), Length (min = 4, max = 10)])
  username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)])
  password = PasswordField('New password', validators = [DataRequired(), Length (min=4, max =10), EqualTo('confirm',message='password does not match')])
- confirm = PasswordField ('Confirm password')
- submit = SubmitField ("Register") 
+ confirm = PasswordField ('Confirm password', validators = [DataRequired(), EqualTo('confirm',message='password does not match')])
+ submit = SubmitField ("Register")
 
