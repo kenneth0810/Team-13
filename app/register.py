6181f8bde17d18baf7e3763b0ca04366e0fa629a
YueYingLee <yueyinglee@gmail.com>
2023-04-28 22:43:34 -0700

    Edited register.py

--- app/register.py
+++ app/register.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class registerUser(FlaskForm):
- fullname = StringField('Full Name', validators = [DataRequired(), Length (min = 4, max = 10)])
- username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)])
+ fullname = StringField('Full Name', validators = [DataRequired(), Length (min = 4, max = 10)], render_kw ={"placeholder":" user1"})
+ username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)], render_kw ={"placeholder":" user1@131.com})
  password = PasswordField('New password', validators = [DataRequired(), Length (min=4, max =10), EqualTo('confirm',message='password does not match')])
  confirm = PasswordField ('Confirm password', validators = [DataRequired(), EqualTo('confirm',message='password does not match')])
  submit = SubmitField ("Register")
