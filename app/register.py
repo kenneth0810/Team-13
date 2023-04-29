e60a3ba37b87025e51c6410ca83a2e5f709d1c15
YueYingLee <yueyinglee@gmail.com>
2023-04-28 22:48:01 -0700

    Fixed error in register.py

--- app/register.py
+++ app/register.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 class registerUser(FlaskForm):
  fullname = StringField('Full Name', validators = [DataRequired(), Length (min = 4, max = 10)], render_kw ={"placeholder":" user1"})
- username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)], render_kw ={"placeholder":" user1@131.com})
+ username = StringField('Username',validators =[DataRequired(), Length (min = 6, max = 15)], render_kw ={"placeholder":" user1@131.com"})
  password = PasswordField('New password', validators = [DataRequired(), Length (min=4, max =10), EqualTo('confirm',message='password does not match')])
  confirm = PasswordField ('Confirm password', validators = [DataRequired(), EqualTo('confirm',message='password does not match')])
  submit = SubmitField ("Register")
